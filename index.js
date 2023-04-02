acb12ed4f63c4e9fd40972ca57571906232b4d21
Marwan Rahim <marwan820@Marwan>
2023-04-02 17:44:51 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun())
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(par2 ="||") {
+    return function(par1 = "Special") {
+      return `You are ${par2}${par1}${par2}!`;
+    }
+  }
+
+    
+
+    
+    
+
+
+
+    
+    
